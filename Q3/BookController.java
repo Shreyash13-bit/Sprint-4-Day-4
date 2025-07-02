d73e3994f35bdb09f5556d5cae5fdb0b1b768781
Shreyash Roy <22052762@kiit.ac.in>
2025-07-02 15:31:42 +0530

    Update BookController.java

--- Q3/BookController.java
+++ Q3/BookController.java
@@ -1,21 +1,15 @@
 package com.example.demo.controller;
-
 import com.example.demo.model.Book;
 import com.example.demo.service.BookService;
 import com.example.demo.exception.BookNotFoundException;
-
 import org.springframework.web.bind.annotation.*;
-
 @RestController
 @RequestMapping("/api/books")
 public class BookController {
-
     private final BookService bookService;
-
     public BookController(BookService bookService) {
         this.bookService = bookService;
     }
-
     @GetMapping("/{id}")
     public Book getBookById(@PathVariable Long id) throws BookNotFoundException {
         return bookService.getBookById(id);
